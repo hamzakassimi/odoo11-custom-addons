15da00a5a336e5015a527c52c931ecd47dee5d3c
hamzakassimi <42745180+hamzakassimi@users.noreply.github.com>
2018-11-06 12:42:30 +0000

    3

--- sprogroup_purchase_request/models/sprogroup_purchase_request.py
+++ sprogroup_purchase_request/models/sprogroup_purchase_request.py
@@ -65,8 +65,8 @@ class SprogroupPurchaseRequest(models.Model):
                              required=True,
                              copy=False,
                              default='draft')
-    warehouse_id = fields.Many2one('warehouse.warehouse','Warehouse project',required=True)
-
+    warehouse_id = fields.Many2one('stock.warehouse','Warehouse project',required=True)
+    commentaire = fields.Text('commentaire')
 
     @api.onchange('state')
     def onchange_state(self):
