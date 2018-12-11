5c76f57e1039a8deeddc913d13acb1415f1de66a
hamzakassimi <42745180+hamzakassimi@users.noreply.github.com>
2018-12-11 17:10:31 +0100

    newcommit

---
+++ sprogroup_purchase_request/models/hr_contract.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing det
+
+
+from odoo import fields, models,api
+from odoo.addons import decimal_precision as dp
+from odoo.tools.translate import _
+from odoo.exceptions import UserError
+
+class hr_contract(models.Model):
+    _inherit = 'hr.contract'
+    etat_contrat =  fields.Selection([('non_legalise', 'Non légalisée'),
+	('legalise', 'Légalisé'),
+	('integration', 'Intégration'),
+	('inactif', 'Inactif')])
